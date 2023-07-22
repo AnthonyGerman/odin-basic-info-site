59b01965e7e0fd662695eede26179e7aff2ae040
AG <Anthony459067@gmail.com>
2023-07-22 04:46:32 -0400

    All 4 pages now show up

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-    fs.readFile('index.html', function(err, data) {
+    let file = '';
+    if (req.url == '/') file = 'index.html';
+    else if (req.url == '/about') file = 'about.html';
+    else if (req.url == '/contact-me') file = 'contact-me.html';
+    else file = '404.html';
+    fs.readFile(file, function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         res.end();
